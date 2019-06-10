d9a6e2a24dd92f829f6fb8b50493edd18f597ebb
Daniel Schneider <daschne8@gmail.com>
2019-06-10 15:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let currentUser = "Crash"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + '!'
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
